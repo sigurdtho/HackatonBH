3591187fbe448c14462717b3efd4f291a6037d8b
Sigurd Thomassen <sigurd14@gmail.com>
2016-03-11 23:00:59 +0100

    Updated activities file

--- YourNextTrip/db/migrate/20160311214343_create_activities.rb
+++ YourNextTrip/db/migrate/20160311214343_create_activities.rb
@@ -1,7 +1,10 @@
 class CreateActivities < ActiveRecord::Migration
   def change
     create_table :activities do |t|
-
+      t.string :title
+      t.string :description
+      t.string :location
+      t.string :image
       t.timestamps null: false
     end
   end
